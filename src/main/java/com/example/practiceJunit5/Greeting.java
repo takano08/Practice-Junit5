8d7b597c17e3c0cab1834c15e00c4481860004e3
takano <ri1saxxminny83xx@gmail.com>
2021-03-02 10:32:02 +0900

    クラス追加

---
+++ src/main/java/com/example/practiceJunit5/Greeting.java
@@ -0,0 +1,13 @@
+package com.example.practiceJunit5;
+
+public class Greeting {
+
+    public String hello() {
+        return "Hello";
+    }
+
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/GreetingTest.java
@@ -0,0 +1,32 @@
+import com.example.practiceJunit5.Greeting;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GreetingTest {
+
+    @DisplayName("method : hello")
+    public class Hello {
+
+        private Greeting test = new Greeting();
+
+        @Test
+        @DisplayName("正常に動作するテストケース")
+        public void case1() {
+            Assertions.assertEquals("Hello", test.hello());
+        }
+    }
+
+    @DisplayName("method : helloWorld")
+    public class HelloWold {
+
+        private Greeting test = new Greeting();
+
+        @Test
+        @DisplayName("正常に動作するテストケース")
+        public void case1() {
+            Assertions.assertEquals("Hello World", test.helloWorld());
+        }
+
+    }
+}
