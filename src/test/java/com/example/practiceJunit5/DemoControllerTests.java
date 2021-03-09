ed4f7099e25342fbb5bfce173114bf113fcf8bdf
takano <ri1saxxminny83xx@gmail.com>
2021-03-09 11:21:58 +0900

    コントローラーのテスト作成。

--- src/test/java/com/example/practiceJunit5/DemoControllerTests.java
+++ src/test/java/com/example/practiceJunit5/DemoControllerTests.java
@@ -7,6 +7,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class DemoControllerTests {
     // Spring MVCのモック
     private MockMvc mockMvc;
